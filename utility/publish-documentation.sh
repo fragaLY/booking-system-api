37c047346f8e932cf647e2d5b098089af21db1ae
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-04-01 19:58:09 +0300

    Fixed Documentation Issues

--- utility/publish-documentation.sh
+++ utility/publish-documentation.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_REPO_SLUG" == "fragaLY/booking-system" ] && [ "$TRAVIS_JDK_VERSION
   git clone --quiet --branch=master https://${GITHUB_TOKEN}@github.com/fragaLY/booking-system master > /dev/null
 
   cd master
-  git rm -rf ./docs/javadoc
+  git rm -rf ./javadoc
   cp -Rf $HOME/javadoc-latest ./docs/javadoc
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to master"
