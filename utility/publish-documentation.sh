b597b697ab10ef3c5b3084747eeec9c6c6327734
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-04-04 18:29:14 +0300

    Clean up

--- utility/publish-documentation.sh
+++ utility/publish-documentation.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if [ "$TRAVIS_REPO_SLUG" == "fragaLY/booking-system" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo "Publishing javadoc..."
