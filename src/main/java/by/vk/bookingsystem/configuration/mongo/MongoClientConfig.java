328e13506552ce156d09c8f18a679c2a75b6d389
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-05-31 12:39:28 +0300

    Cloud Client updated. Removed secured settings.

--- src/main/java/by/vk/bookingsystem/configuration/mongo/MongoClientConfig.java
+++ src/main/java/by/vk/bookingsystem/configuration/mongo/MongoClientConfig.java
@@ -5,21 +5,21 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
- * The config for development profile of mongodb
+ * The config for mongodb client
  *
  * @author Vadzim_Kavalkou
  */
 @Configuration
-@Profile("development")
 @Service
-public class DevelopmentMongoConfig {
+@RefreshScope
+public class MongoClientConfig {
 
   /**
    * Configures the client to work with data in cloud storage
